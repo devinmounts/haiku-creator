99d58408a3e6070001118140e427800ccaa21e21
Craig Wann and Devin Mounts <craig@>
2018-08-01 15:48:40 -0700

    create edge case for words greater than three letters ending in e.

--- src/haiku.js
+++ src/haiku.js
@@ -7,9 +7,9 @@
 // }
 export class Haiku{
   constructor(line1, line2, line3) {
-    this.line1 = line1.replace(/[.?',/#!$%^&*;:{}=\-_`~()]/g,"").replace(/s{2,}/g," ").split(" ");
-    this.line2 = line2.replace(/[.?',/#!$%^&*;:{}=\-_`~()]/g,"").replace(/s{2,}/g," ").split(" ");
-    this.line3 = line3.replace(/[.?',/#!$%^&*;:{}=\-_`~()]/g,"").replace(/s{2,}/g," ").split(" ");
+    this.line1 = line1.replace(/[.?',/#!$%^&*;:{}=\-_`~()]/g,"").toLowerCase().split(" ");
+    this.line2 = line2.replace(/[.?',/#!$%^&*;:{}=\-_`~()]/g,"").toLowerCase().split(" ");
+    this.line3 = line3.replace(/[.?',/#!$%^&*;:{}=\-_`~()]/g,"").toLowerCase().split(" ");
     this.letters = {
       y: "y",
       vowels: ["a", "e", "i", "o", "u"]
@@ -24,6 +24,9 @@ export class Haiku{
   }
   countVowelsLine1() {
     for (let i = 0; i < this.line1.length; i++){
+      if (this.line1[i].endsWith("e") && this.line1[i].length > 3) {
+        this.line1Result -= 1;
+      }
       for (let k = 0; k < this.line1[i].length; k++) {
         if(this.letters.vowels.includes(this.line1[i][k])){
           this.line1Result ++;
@@ -33,15 +36,24 @@ export class Haiku{
   }
   countVowelsLine2() {
     for (let i = 0; i < this.line2.length; i++){
+      if (this.line2[i].endsWith("e") && this.line2[i].length > 3) {
+        this.line2Result -= 1;
+      }
       for (let k = 0; k < this.line2[i].length; k++) {
         if(this.letters.vowels.includes(this.line2[i][k])){
           this.line2Result ++;
+        }
+        if (this.line2[i].endsWith("e")) {
+          this.line2Result -= 1;
         }
       }
     }
   }
   countVowelsLine3() {
     for (let i = 0; i < this.line3.length; i++){
+      if (this.line3[i].endsWith("e") && this.line3[i].length > 3) {
+        this.line3Result -= 1;
+      }
       for (let k = 0; k < this.line3[i].length; k++) {
         if(this.letters.vowels.includes(this.line3[i][k])){
           this.line3Result ++;
