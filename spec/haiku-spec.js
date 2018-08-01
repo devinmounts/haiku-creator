55428dbc88c9144e7d098cdddf084866c9f840d4
Craig Wann and Devin Mounts <devin@>
2018-08-01 16:44:39 -0700

    end of day.

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -4,18 +4,18 @@ describe('Haiku', function() {
   var reusableHaiku;
 
   beforeEach(function() {
-    reusableHaiku = new Haiku("the fresh green grass talks", "it says hello and good day", "I smile and walk on");
+    reusableHaiku = new Haiku("recipe fresh green grass talks", "it says hello and good day", "I smile and walk on");
   });
 
   it('should test whether a haiku has three lines', function(){
-    expect(reusableHaiku.line1).toEqual(["the", "fresh", "green", "grass", "talks"]);
+    expect(reusableHaiku.line1).toEqual(["recipe", "fresh", "green", "grass", "talks"]);
     expect(reusableHaiku.line2).toEqual(["it", "says", "hello", "and", "good", "day"]);
-    expect(reusableHaiku.line3).toEqual(["I", "smile", "and", "walk", "on"]);
+    expect(reusableHaiku.line3).toEqual(["i", "smile", "and", "walk", "on"]);
   });
 
   it('should test countVowelsLine1() method returns number of vowels', function() {
     reusableHaiku.countVowelsLine1();
-    expect(reusableHaiku.line1Result).toEqual(6);
+    expect(reusableHaiku.line1Result).toEqual(8);
   });
   it('should test countVowelsLine2() method returns number of vowels', function() {
     reusableHaiku.countVowelsLine2();
