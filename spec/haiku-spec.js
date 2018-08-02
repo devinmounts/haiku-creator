ecb8b7e02dc0170c74d982b36546c5862e54d348
Craig Wann and Devin Mounts <craig@>
2018-08-02 11:33:28 -0700

    create ends with E method.

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -4,26 +4,36 @@ describe('Haiku', function() {
   var reusableHaiku;
 
   beforeEach(function() {
-    reusableHaiku = new Haiku("recipe fresh green grass talks", "it says hello and good day", "I smile and walk on");
+    reusableHaiku = new Haiku("recipe fresh green grass talks", "it says green and good day", "I smile and walk on");
   });
 
   it('should test whether a haiku has three lines', function(){
     expect(reusableHaiku.line1).toEqual(["recipe", "fresh", "green", "grass", "talks"]);
-    expect(reusableHaiku.line2).toEqual(["it", "says", "hello", "and", "good", "day"]);
+    expect(reusableHaiku.line2).toEqual(["it", "says", "green", "and", "good", "day"]);
     expect(reusableHaiku.line3).toEqual(["i", "smile", "and", "walk", "on"]);
   });
 
-  it('should test countVowelsLine1() method returns number of vowels', function() {
-    reusableHaiku.countVowelsLine1();
-    expect(reusableHaiku.line1Result).toEqual(8);
-  });
-  it('should test countVowelsLine2() method returns number of vowels', function() {
-    reusableHaiku.countVowelsLine2();
-    expect(reusableHaiku.line2Result).toEqual(8);
+  it('should test vowelsCounter() method updates lineResult', function() {
+    reusableHaiku.vowelsCounter(reusableHaiku.line1);
+    expect(reusableHaiku.lineResult).toEqual(8);
   });
 
-  it('should test countVowelsLine3() method returns number of vowels', function() {
-    reusableHaiku.countVowelsLine3();
-    expect(reusableHaiku.line3Result).toEqual(5);
+  it('should test endsWithE() method updates lineResult for silent e', function() {
+    reusableHaiku.vowelsCounter(reusableHaiku.line3);
+    reusableHaiku.endswithE(reusableHaiku.line3);
+    expect(reusableHaiku.lineResult).toEqual(5);
+  });
+
+  it('should test endsWithE() method updates lineResult for exceptions', function() {
+    reusableHaiku.vowelsCounter(reusableHaiku.line1);
+    reusableHaiku.endswithE(reusableHaiku.line1);
+    expect(reusableHaiku.lineResult).toEqual(8);
+  });
+
+  it('should test phthongCheck() method updates lineResult for repeating vowels', function() {
+    reusableHaiku.vowelsCounter(reusableHaiku.line2);
+    reusableHaiku.endswithE(reusableHaiku.line2);
+    reusableHaiku.phthongCheck(reusableHaiku.line2);
+    expect(reusableHaiku.lineResult).toEqual(6);
   });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,10 +1,3 @@
-// function new_count(word) {
-//   word = word.toLowerCase();
-//   if(word.length <= 3) { return 1; }
-//     word = word.replace
-//
-//
-// }
 export class Haiku{
   constructor(line1, line2, line3) {
     this.line1 = line1.replace(/[.?',/#!$%^&*;:{}=\-_`~()]/g,"").toLowerCase().split(" ");
@@ -13,7 +6,8 @@ export class Haiku{
     this.letters = {
       y: "y",
       vowels: ["a", "e", "i", "o", "u"],
-      exceptions: ["recipe", "catastrophe", "resume", "toupee", "puree", "recipe", "catastrophe", "apostrophe", "acme", "acne", "ante", "boise", "maybe", "possee", "adobe", "apache", "karate", "peyote", "reveille", "sesame", "shoshone", "syncope", "vigilante", "hyperbole", "coyote"]
+      exceptions: ["recipe", "catastrophe", "resume", "toupee", "puree", "recipe", "catastrophe", "apostrophe", "acme", "acne", "ante", "boise", "maybe", "possee", "adobe", "apache", "karate", "peyote", "reveille", "sesame", "shoshone", "syncope", "vigilante", "hyperbole", "coyote"],
+      diphthongs: ["ee", "ai", "ay", "ea", "ie", "ei", "oo", "ou", "oe", "ue", "ey", "ay", "oy", "oi", "au"]
     }
     this.lineResult = 0;
 
@@ -21,10 +15,10 @@ export class Haiku{
 
   }
   vowelsCounter(line) {
-    for (let i = 0; i < this.line1.length; i++){
-      for (let k = 0; k < this.line1[i].length; k++) {
-        if(this.letters.vowels.includes(this.line1[i][k])){
-          this.line1Result ++;
+    for (let i = 0; i < line.length; i++){
+      for (let k = 0; k < line[i].length; k++) {
+        if(this.letters.vowels.includes(line[i][k])){
+          this.lineResult++;
         }
       }
     }
@@ -32,42 +26,30 @@ export class Haiku{
 
   endswithE(line){
     for (let i = 0; i < line.length; i++) {
-      if (this.letters.exceptions.includes(line[i])) {
-        this.lineResult + 0
+      if ((line[i].endsWith("e") && line[i].length > 3) && (!this.letters.exceptions.includes(line[i]))) {
+        this.lineResult -= 1;
       }
-
     }
   }
-  if (this.letters.exceptions.includes(this.line1[i])){
 
-  } else if (this.line1[i].endsWith("e") && this.line1[i].length > 3) {
-    this.line1Result -= 1;
-  // countVowelsLine2() {
-  //   for (let i = 0; i < this.line2.length; i++){
-  //     if (this.line2[i].endsWith("e") && this.line2[i].length > 3) {
-  //       this.line2Result -= 1;
-  //     }
-  //     for (let k = 0; k < this.line2[i].length; k++) {
-  //       if(this.letters.vowels.includes(this.line2[i][k])){
-  //         this.line2Result ++;
-  //       }
-  //       if (this.line2[i].endsWith("e")) {
-  //         this.line2Result -= 1;
-  //       }
-  //     }
-  //   }
-  // }
-  // countVowelsLine3() {
-  //   for (let i = 0; i < this.line3.length; i++){
-  //     if (this.line3[i].endsWith("e") && this.line3[i].length > 3) {
-  //       this.line3Result -= 1;
-  //     }
-  //     for (let k = 0; k < this.line3[i].length; k++) {
-  //       if(this.letters.vowels.includes(this.line3[i][k])){
-  //         this.line3Result ++;
-  //       }
-  //     }
-  //   }
-  // }
-  //
-  // }
+  phthongCheck(line){
+    for ( let i = 0; i < line.length; i++) {
+      if(line[i].length > 3){
+        let k=0;
+        let i=0;
+        while(k < line[i].length){
+          if ((this.letters.vowels.includes(line[i][k])) && (this.letters.vowels.includes(line[i][k+1])) && (this.letters.vowels.includes(line[i][k+2]))) {
+            this.lineResult--;
+            k+=2;
+          } else if ((this.letters.vowels.includes(line[i][k])) && (this.letters.vowels.includes(line[i][k+1]))){
+            this.lineResult--;
+            k+=1;
+          }
+          else {
+            k+=1;
+          }
+        }
+      }
+    }
+  }
+}
